6d8910739f73c65389d0239640be90f0b58a8886
erroldepa <errold.epa@gmail.com>
2025-10-29 00:27:51 +0800

    re-run build

--- .medusa/server/public/admin/assets/chunk-BDZE3G4Q-B7jhGs0w.js
+++
@@ -1,1 +0,0 @@
-import{aO as i}from"./index-bzW-SAbR.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
--- .medusa/server/public/admin/assets/chunk-F6ZOHZVB-DQG38AnX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-bzW-SAbR.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
