1e723664b3fbcd1e8dd57c55a6ca7c06715a64ea
erroldepa <errold.epa@gmail.com>
2025-10-29 19:34:19 +0800

    add path

--- medusa-config.ts
+++ medusa-config.ts
@@ -18,6 +18,7 @@ module.exports = defineConfig({
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
     backendUrl: process.env.MEDUSA_BACKEND_URL, // for Admin config
+    path: process.env.MEDUSA_ADMIN_PATH || "/app",
   },
   modules: [
     {
